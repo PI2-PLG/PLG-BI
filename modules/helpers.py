9e8fc6fd4cd19415ceb2ac81d52664fe97bd0268
Lucas S <lucas.soaresouza@gmail.com>
2019-11-27 18:35:42 -0300

    Getting the lest lat and long in case de offline status

--- modules/helpers.py
+++ modules/helpers.py
@@ -16,6 +16,19 @@ def isOffline(module):
     try:
         if(module.module_data.last().latitude == 0.0 and module.module_data.last().longitude == 0.0):
             status = 'OFFLINE'
+            '''
+                Como os valores de longitude e latitude foram iguais a 0
+                o sistema busca o ultimo valor de longitude e latitude como
+                localização atual do módulo
+            '''
+            last_data_id = module.module_data.last().id
+            last_data = ModuleData.objects.get(id=last_data_id)
+            all_data = module.module_data.all()
+            for data in all_data:
+                if(data.longitude != 0 and data.latitude != 0):
+                    last_data.latitude = data.latitude
+                    last_data.longitude = data.longitude
+            last_data.save()
             setStatus(module, status)
         else:
             status = 'ONLINE'
